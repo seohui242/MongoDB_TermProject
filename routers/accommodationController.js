1148e79038cccbb02b8b70e31814cfbc34599f77
seohui242 <117262212+seohui242@users.noreply.github.com>
2023-12-11 20:20:34 +0900

    2번 완

--- routers/accommodationController.js
+++ routers/accommodationController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { Accommodation } = require("../models/accommodation");
+const { Reservation } = require("../models/reservation");
 
 router.post("/", async (req, res) => {
   try {
@@ -18,6 +19,26 @@ router.get("/", async (req, res) => {
   try {
     const accommodations = await Accommodation.find({});
     return res.send({ lodgings });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send({ err: err.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const accommodation = await Accommodation.findById(req.params.id);
+    if (!accommodation) return res.status(404).send({ err: 'Accommodation not found' });
+
+    const reservations = await Reservation.find({ accommodation: req.params.id });
+    const ratings = reservations.map(reservation => reservation.starRate).filter(Boolean);
+    const averageRating = ratings.reduce((a, b) => a + b, 0) / ratings.length || 'No ratings yet';
+
+    return res.send({
+      accommodation,
+      averageRating,
+      reservations
+    });
   } catch (err) {
     console.log(err);
     return res.status(400).send({ err: err.message });
